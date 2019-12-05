be9b0af6270a1f5cfdaa22b6b5b8a42e08618ec0
Ziphil <yfe23457@nifty.com>
2019-12-05 20:32:21 +0900

    リファクタリング

--- source/Ziphil/Language/UntypedLambda/Parse.hs
+++ source/Ziphil/Language/UntypedLambda/Parse.hs
@@ -15,10 +15,10 @@ import Ziphil.Language.UntypedLambda.Base
 
 
 parseTerm :: String -> Either ParseError (WithContext Term)
-parseTerm text = uncurry (:-) <$> parse (getTerm []) "" text
+parseTerm text = parse (getTerm []) "" text
 
 parseTerm' :: String -> Either ParseError Term
-parseTerm' text = snd <$> parse (getTerm []) "" text
+parseTerm' text = (\(context :- term) -> term) <$> parse (getTerm []) "" text
 
 makeTerm :: String -> WithContext Term
 makeTerm = fromRight (["?"] :- Var Info 0) . parseTerm
@@ -26,17 +26,17 @@ makeTerm = fromRight (["?"] :- Var Info 0) . parseTerm
 makeTerm' :: String -> Term
 makeTerm' = fromRight (Var Info 0) . parseTerm'
 
-getTerm :: Context -> Parser (Context, Term)
+getTerm :: Context -> Parser (WithContext Term)
 getTerm context = try (getVarTerm context) <|> try (getAbsTerm context) <|> try (getAppTerm context)
 
-getVarTerm :: Context -> Parser (Context, Term)
+getVarTerm :: Context -> Parser (WithContext Term)
 getVarTerm context = do
   name <- getVarName
   case fetchIndex context name of
-    Just index -> return (context, Var Info index)
-    Nothing -> return (context ++ [name], Var Info (length context))
+    Just index -> return (context :- Var Info index)
+    Nothing -> return ((context ++ [name]) :- Var Info (length context))
 
-getAbsTerm :: Context -> Parser (Context, Term)
+getAbsTerm :: Context -> Parser (WithContext Term)
 getAbsTerm context = do
   char '('
   getSpaces
@@ -46,23 +46,22 @@ getAbsTerm context = do
   getSpaces
   char '.'
   getSpaces
-  (contContext, contTerm) <- getTerm (name : context)
+  contContext :- contTerm <- getTerm (name : context)
   getSpaces
   char ')'
   case viaNonEmpty tail contContext of
-    Just finalContext -> return (finalContext, Abs Info name contTerm)
+    Just finalContext -> return (finalContext :- Abs Info name contTerm)
     Nothing -> fail "weird"
 
-getAppTerm :: Context -> Parser (Context, Term)
+getAppTerm :: Context -> Parser (WithContext Term)
 getAppTerm context = do
   char '('
+  funcContext :- funcTerm <- getTerm context
   getSpaces
-  (funcContext, funcTerm) <- getTerm context
-  getSpaces
-  (valContext, valTerm) <- getTerm funcContext
+  valContext :- valTerm <- getTerm funcContext
   getSpaces
   char ')'
-  return (valContext, App Info funcTerm valTerm)
+  return (valContext :- App Info funcTerm valTerm)
 
 getVarName :: Parser VarName
 getVarName = do
